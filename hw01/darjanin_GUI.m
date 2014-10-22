3eaaf790500f369a3d6de1e8b98683dbaee3bf15
Milan Darjanin <me@milandarjanin.com>
2014-10-22 12:17:16 +0200

    Added edge detection code; started refactoring

--- hw01/darjanin_GUI.m
+++ hw01/darjanin_GUI.m
@@ -22,7 +22,7 @@ function varargout = darjanin_GUI(varargin)
 
 % Edit the above text to modify the response to help darjanin_GUI
 
-% Last Modified by GUIDE v2.5 19-Oct-2014 14:43:48
+% Last Modified by GUIDE v2.5 22-Oct-2014 12:07:10
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -55,11 +55,13 @@ function darjanin_GUI_OpeningFcn(hObject, eventdata, handles, varargin)
 handles.output = hObject;
 
 handles.rgb = ones(3);
+handles.input = ones(1);
 handles.result = ones(1);
 
 handles.smooth_filter_size = 3;
+handles.edge_treshold = 0.1;
 
-imshow(handles.rgb, 'Parent', handles.axes1);
+imshow(handles.input, 'Parent', handles.axes1);
 imshow(handles.result, 'Parent', handles.axes2);
 
 % Update handles structure
@@ -93,6 +95,8 @@ function menu_item_load_image_Callback(hObject, eventdata, handles)
 % hObject    handle to menu_item_load_image (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% file_formats list of all supported formats
 file_formats = {
     '*.jpg', 'JPEG imagefile (*.jpg)';
     '*.png', 'PNG imagefile (*.png)';
@@ -103,13 +107,14 @@ if ~isequal(i_file, 0)
     i_file = fullfile(i_pathname, i_file);
     i_rgb = double(imread(i_file))/255;
     handles.rgb = i_rgb;
+    handles.input = i_rgb;
     handles.result = i_rgb;
     [idx_im, handles.map] = rgb2ind(i_rgb, 256);
     handles.index_image = idx_im;
     handles.current_map = handles.map;
     % show original image in the first axes
-    imshow(i_rgb,'Parent',handles.axes1);
-    imshow(i_rgb,'Parent',handles.axes2);
+    imshow(handles.input, 'Parent', handles.axes1);
+    imshow(handles.input, 'Parent', handles.axes2);
     
 end
 
@@ -123,8 +128,6 @@ function popup_smooth_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: contents = cellstr(get(hObject,'String')) returns popup_smooth contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popup_smooth
 str = get(hObject, 'String');
 val = get(hObject, 'Value');
 
@@ -139,7 +142,7 @@ function result_im = smooth_im(im, method, filter_size)
 % result_im     result
 % im            input image
 % method        method used for smoothing
-% filter_size   size of filter defined in gui, if nothing specified than 1
+% filter_size   size of filter defined in gui, if nothing specified than 3
 switch method;
 case 'average'
     h = fspecial('average', filter_size);
@@ -155,8 +158,6 @@ function popup_smooth_CreateFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -168,9 +169,7 @@ function edit_filter_size_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of edit_filter_size as text
-%        str2double(get(hObject,'String')) returns contents of edit_filter_size as a double
-value = str2num(get(hObject, 'String'));
+value = str2double(get(hObject, 'String'));
 if (value > 0)
     handles.smooth_filter_size = value;
 end
@@ -184,56 +183,8 @@ function edit_filter_size_CreateFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on selection change in popupmenu2.
-function popupmenu2_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu2 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu2
-
-
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu2_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on slider movement.
-function slider2_Callback(hObject, eventdata, handles)
-% hObject    handle to slider2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-
-
-% --- Executes during object creation, after setting all properties.
-function slider2_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to slider2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: slider controls usually have a light gray background.
-if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
 
@@ -242,10 +193,8 @@ function popup_noise_Callback(hObject, eventdata, handles)
 % hObject    handle to popup_noise (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns popup_noise contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popup_noise
 % Determine the selected noise value
+
 str = get(hObject, 'String');
 val = get(hObject, 'Value');
 
@@ -262,53 +211,82 @@ function popup_noise_CreateFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
 
 % --- Executes on slider movement.
-function slider3_Callback(hObject, eventdata, handles)
-% hObject    handle to slider3 (see GCBO)
+function slider_treshold_Callback(hObject, eventdata, handles)
+% hObject    handle to slider_treshold (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+handles.edge_treshold = get(hObject, 'Value');
+
+disp(handles.edge_treshold);
+
+guidata(hObject, handles);
 
 
 % --- Executes during object creation, after setting all properties.
-function slider3_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to slider3 (see GCBO)
+function slider_treshold_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider_treshold (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-% Hint: slider controls usually have a light gray background.
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
 
-% --- Executes on selection change in popupmenu6.
-function popupmenu6_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu6 (see GCBO)
+% --- Executes on selection change in popup_edge_detection.
+function popup_edge_detection_Callback(hObject, eventdata, handles)
+% hObject    handle to popup_edge_detection (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu6 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu6
+%imshow(edge(handles.result,'sobel'), 'Parent', handles.axes2);
+str = get(hObject, 'String');
+val = get(hObject, 'Value');
+
+handles.result = edgeDetection(handles.result, str{val}, handles.edge_treshold);
+
+imshow(handles.result, 'Parent', handles.axes2);
+
+guidata(hObject, handles);
+
+function result_im = edgeDetection(im, method, threshold)
+
+switch method;
+case 'sobel' 
+    Sx = [1 2 1; 0 0 0; -1 -2 -1];
+    Sy = [1 0 -1; 2 0 -2; 1 0 -1];
+case 'prewitt'
+    Sx = [-1 0 1; -1 0 1; -1 0 1];
+    Sy = [-1 -1 -1; 0 0 0; 1 1 1];
+case 'roberts'
+    Sx = [1 0; 0 -1];
+    Sy = [0 1; -1 0];
+end
+
+
+H = conv2(double(im), Sx, 'same');
+V = conv2(double(im), Sy, 'same');
+E = sqrt(H.*H + V.*V);
+whos E;
+edge_image = uint8((E > threshold) * 255);
+
+result_im = edge_image;
+
 
 
 % --- Executes during object creation, after setting all properties.
-function popupmenu6_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu6 (see GCBO)
+function popup_edge_detection_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popup_edge_detection (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -319,8 +297,8 @@ function btn_grayscale_Callback(hObject, eventdata, handles)
 % hObject    handle to btn_grayscale (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-handles.gray = rgb2gray(handles.rgb);
-handles.result = handles.gray;
+gray = rgb2gray(handles.rgb);
+handles.result = gray;
 
 imshow(handles.result, 'Parent', handles.axes2);
 
@@ -328,3 +306,39 @@ imshow(handles.result, 'Parent', handles.axes2);
 guidata(hObject, handles);
 
 
+% --------------------------------------------------------------------
+function menu_edit_image_Callback(hObject, eventdata, handles)
+% hObject    handle to menu_edit_image (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function menu_item_grayscale_Callback(hObject, eventdata, handles)
+% hObject    handle to menu_item_grayscale (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+gray = rgb2gray(handles.rgb);
+handles.input = gray;
+handles.result = gray;
+
+imshow(handles.input, 'Parent', handles.axes1);
+imshow(handles.result, 'Parent', handles.axes2);
+
+% Update handles structure
+guidata(hObject, handles);
+
+
+% --------------------------------------------------------------------
+function menu_item_rgb_Callback(hObject, eventdata, handles)
+% hObject    handle to menu_item_rgb (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+handles.input = handles.rgb;
+handles.result = handles.rgb;
+
+imshow(handles.input, 'Parent', handles.axes1);
+imshow(handles.result, 'Parent', handles.axes2);
+
+% Update handles structure
+guidata(hObject, handles);
